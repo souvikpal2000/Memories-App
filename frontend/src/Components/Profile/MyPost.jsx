6b779c9311bcb59f96dcb46eae6209310cef5958
souvikpal2000 <spal62588@gmail.com>
2022-11-14 11:55:00 +0530

    Styles Applied

--- frontend/src/Components/Profile/MyPost.jsx
+++ frontend/src/Components/Profile/MyPost.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 
 const MyPost = ({memories}) => {
     return(
         <>
-            {memories.map((memory, index) => {
-                return(
-                    <div key={index}>
-                        <img src={memory.imageDetails.base64} alt={memory.imageDetails.name} />
-                    </div>     
-                )
-            })}
+            <Container>
+                <div className="postsContainer">
+                    {memories.map((memory, index) => {
+                        return(
+                            <div key={index} className="imgContainer">
+                                <img src={memory.imageDetails.base64} alt={memory.imageDetails.name} className="post"/>
+                            </div>   
+                        )
+                    })}
+                </div>
+            </Container>
         </>
     )
 }
