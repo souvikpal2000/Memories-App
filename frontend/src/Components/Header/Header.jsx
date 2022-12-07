682a56d2aeda25738321f75a9d06b4e573401e3f
souvikpal2000 <spal62588@gmail.com>
2022-12-07 12:17:58 +0530

    AutoClose Navbar Done

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext }  from "react";
+import React, { useContext, useState }  from "react";
 import { NavLink } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -10,26 +10,30 @@ import "./style.css";
 
 const Header = () => {
     const {state} = useContext(UserContext);
+    const [expanded, setExpanded] = useState(false); 
 
     const Menus = () => {
+        const closeNavbar = () => {
+            setExpanded(false);
+        }
+
         if(!state){
             return(
                 <>
-                    <NavLink to="/" id="menu" className="nav-link">Home</NavLink>
-                    <NavLink to="/signup" id="menu" className="nav-link">Signup</NavLink>
-                    <NavLink to="/login" id="menu" className="nav-link">Login</NavLink>
+                    <NavLink to="/" id="menu" className="nav-link" onClick={closeNavbar}>Home</NavLink>
+                    <NavLink to="/signup" id="menu" className="nav-link" onClick={closeNavbar}>Signup</NavLink>
+                    <NavLink to="/login" id="menu" className="nav-link" onClick={closeNavbar}>Login</NavLink>
                 </>
             )
-
         }
         return(
             <>
-                <NavLink to="/" id="menu" className="nav-link">Home</NavLink>
-                <NavLink to="/posts" id="menu" className="nav-link">Posts</NavLink>
+                <NavLink to="/" id="menu" className="nav-link" onClick={closeNavbar}>Home</NavLink>
+                <NavLink to="/posts" id="menu" className="nav-link" onClick={closeNavbar}>Posts</NavLink>
                 <NavDropdown id="nav-dropdown-dark-example" title="Me" menuVariant="dark" className="navbarLink">
-                    <NavLink to="/profile" id="menu" className="dropdown-item">View Profile</NavLink>
+                    <NavLink to="/profile" id="menu" className="dropdown-item" onClick={closeNavbar}>View Profile</NavLink>
                     <NavDropdown.Divider />
-                    <NavLink to="/logout" id="menu" className="dropdown-item">Logout</NavLink>
+                    <NavLink to="/logout" id="menu" className="dropdown-item" onClick={closeNavbar}>Logout</NavLink>
                 </NavDropdown>
             </>
         )
@@ -37,12 +41,12 @@ const Header = () => {
 
     return(
         <>
-            <Navbar expand="lg" className="navContainer">
+            <Navbar expand="lg" className="navContainer" expanded={expanded}>
                 <Container>
                     <Navbar.Brand>
                         <img src={logo} alt="" className="brandImage"/>
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggler"/>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggler" onClick={() => setExpanded(!expanded)}/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
                             <Menus/>
