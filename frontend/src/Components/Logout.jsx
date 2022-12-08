a07b343df007d2e474c794ae1802dd19c5700ed0
souvikpal2000 <spal62588@gmail.com>
2022-12-08 10:06:20 +0530

    Logout Optimization Done

--- frontend/src/Components/Logout.jsx
+++ frontend/src/Components/Logout.jsx
@@ -1,24 +1,16 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 import { UserContext } from "../App";
 
 const Logout = () => {
     const {dispatch} = useContext(UserContext);
     const navigate = useNavigate();
     useEffect(() => {
-        fetch("/logout", {
-            method: "GET",
-            headers:{
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            credentials: "include"
-        }).then((res) => {
-            dispatch({type: "loggedOut"});
-            navigate("/");
-        }).catch((err) => {
-            console.log("Error 503 : Service Unavailable");
-        })
+        Cookies.remove('jwt');
+        Cookies.remove('loggedInModal');
+        dispatch({type: "loggedOut"});
+        navigate("/");
     }, []);
 }
 
